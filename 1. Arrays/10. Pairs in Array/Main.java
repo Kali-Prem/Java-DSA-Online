5e873ec4a08e93643bf656430d71459903b3a7b2
Prem Kumar <205642704+Kali-Prem@users.noreply.github.com>
2026-04-03 23:32:09 +0530

    Pairs in array Added

--- 1. Arrays/10. Pairs in Array/Main.java
+++ 1. Arrays/10. Pairs in Array/Main.java
@@ -6,12 +6,16 @@ public class Main {
     }
     public static void printPairs(int num[]){
         int n = num.length;
+        int tp = 0; //total pairs also can be calculated
         for(int i = 0; i < n; i++){
             for(int j = i + 1; j < n; j++){
-                System.out.print("(" + num[i] + num[j]+")" + " ");
+                System.out.print("(" + num[i] + "," + num[j]+")" + " ");
+                tp++;
 
             }
+            System.out.println();
         }
+        System.out.print("Total pairs : "+ tp);
 
     }
 }
