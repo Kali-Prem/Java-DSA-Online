47c1b31391fe2c1a090f73566db350f9ffc343f8
Prem Kumar <205642704+Kali-Prem@users.noreply.github.com>
2026-04-30 09:43:33 +0530

    Update main.java

--- 3. Hashing/1. HashMap/1. Hashmap Operations/main.java
+++ 3. Hashing/1. HashMap/1. Hashmap Operations/main.java
@@ -18,16 +18,42 @@ public class main{
         // cREATE
         HashMap<String, Integer> hm = new HashMap<>();              // Memonry ke andr khali HASHMAP BN GYA HAI 
 
-        //Insert
+        //Insert - O(1)
+        // Store Data Randomly
         hm.put("India", 150);
         hm.put("China", 140);
         hm.put("Kali", 40);
+        hm.put("USA", 120);
 
         System.out.println(hm);
 
-        //Get
-        System.out.print(hm.get("India"));
+        //Get - O(1)
+        int population = hm.get("China");
+        System.out.println(population);
 
+        System.out.println(hm.get("India"));
+        System.out.println(hm.get("Kali"));
+
+        System.out.println(hm.get("Indonesia")); // return - null value because the key is not present in tha map
         
+
+        // ContainsKey(key) - O(1)
+        System.out.println(hm.containsKey("India"));     //Return -  True
+        System.out.println(hm.containsKey("Indonesia")); // Return -False
+        
+
+        // Remove 
+        hm.remove("China");
+        System.out.println(hm);  
+
+        // Size
+        System.out.println(hm.size());
+
+        // IsEmpty
+        System.out.println(hm.isEmpty()); //False
+
+        // Clear
+        hm.clear(); //Clear the map means remove all the data of map
+        System.out.println(hm);
     }
 }
